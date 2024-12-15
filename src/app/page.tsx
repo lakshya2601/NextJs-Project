d449b54e95c8a09c477cae7df30fbaa594f6d880
lakshyactruh <lakshya@ctruh.com>
2024-12-15 15:15:45 +0530

    web-analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,10 @@ import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <SpeedInsights />
       <HeroSection />
       <FeaturedCourses />
       <WhyChooseUs />
